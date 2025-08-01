d5db7eee15528eaea8459fc9d37df80f11179107
paulotitec <paulo.titec@gmail.com>
2025-08-01 11:18:19 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60){ //condição simples!.
+    console.log(`Voce ultrapassou avelocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('voce e brasileiro!')
+}
+else{
+    console.log('voce e estrangeiro!')
+}
