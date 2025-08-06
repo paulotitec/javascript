404ec8f5aabbe02f6d047b6274533be1adbc389f
paulotitec <paulo.titec@gmail.com>
2025-08-06 09:54:40 -0300

    fm at aula 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while(c <= 6)
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
